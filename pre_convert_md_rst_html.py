cc99687fc8229f4f32bfe773d592adb8b752efb1
HatsuneMiku <999hatsune@gmail.com>
2014-07-12 17:48:38 +0900

    release 0.0.2

---
+++ pre_convert_md_rst_html.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+'''pre_convert_md_rst_html
+pandoc and pyandoc
+.md -> .rst -> PKG-INFO (PyPI)
+         |-> filter (add css header / sourceCode -> literal-block) -> html
+'''
+
+import sys, os
+import re
+import pandoc
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
